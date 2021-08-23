50e5c272f1323aca07dc7403d3bc16dea4fc380d
Anyesh <sthacruz@gmail.com>
2021-08-23 11:28:22 +0545

    feat: Add usagelogger

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 // Imports
 import express from "express";
+import { HttpLoggerForExpress } from "resurfaceio-logger";
 import setupGraphQL from "./setup/graphql";
 // App Imports
 import setupLoadModules from "./setup/loader";
 import setupStartServer from "./setup/server";
-
 // Create express server
 const server = express();
+
+HttpLoggerForExpress.add(server, {
+  rules: "include debug",
+});
 
 server.get("/ping", function (request, response) {
   response.status(200).json({ msg: "pong" });
