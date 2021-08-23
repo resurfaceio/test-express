63c59d5524ae7c51238612e2afa5cc08bffff952
Anyesh <sthacruz@gmail.com>
2021-08-23 10:33:15 +0545

    feat: Change server to use GraphQL

--- index.js
+++ index.js
@@ -1,61 +1,22 @@
-const express = require('express');
-const session = require('express-session');
+// Imports
+import express from "express";
+import setupGraphQL from "./setup/graphql";
+// App Imports
+import setupLoadModules from "./setup/loader";
+import setupStartServer from "./setup/server";
 
-const app = express();
-app.set('port', (process.env.PORT || 5000));
+// Create express server
+const server = express();
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({extended: true})); // support encoded bodies
-
-app.use(session({ secret: 'this-is-a-secret-token', cookie: { maxAge: 60000 }}));
-
-// add Resurface after body/session middleware
-const resurfaceio = require('resurfaceio-logger');
-resurfaceio.HttpLoggerForExpress.add(app, {
-    rules: 'include debug'
+server.get("/ping", function (request, response) {
+  response.status(200).json({ msg: "pong" });
 });
 
-app.get('/', function (request, response) {
-    response.status(200).send('Hello from Express');
-});
+// Setup load modules
+setupLoadModules(server);
 
-app.post('/', function (request, response) {
-    let html = '<html><p>Posted to Express:</p>';
-    const body = request.body;
-    for (const key in body) {
-        if (body.hasOwnProperty(key)) {
-            html += `<p>${key}:${body[key]}</p>`;
-        }
-    }
-    html += '</html>\n';
-    response.status(200).send(html);
-});
+// Setup GraphQL
+setupGraphQL(server);
 
-app.get('/ping', function (request, response) {
-    response.status(200).json({msg: 'pong'});
-});
-
-app.get('/send_array', function (request, response) {
-    response.status(200).send([1, 2, 3]);
-});
-
-app.get('/send_basic', function (request, response) {
-    response.status(200).send('whoop1');
-});
-
-app.get('/send_buffer', function (request, response) {
-    response.status(200).send(new Buffer('nail the door shut'));
-});
-
-app.get('/write', function (request, response) {
-    response.status(200);
-    response.type('text/plain');
-    response.write('hello');
-    response.write(' world');
-    response.end();
-});
-
-app.listen(app.get('port'), function () {
-    console.log('Node app is running on port', app.get('port'));
-});
+// Start server
+setupStartServer(server);
